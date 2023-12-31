6fb5cd5af0dcc75988d3236c22008dc46ad105a6
Mateus Rodrigues <mateus3107@hotmail.com>
2023-12-31 10:40:19 -0300

    Teste Configuration AppSettings Azure

--- Program.cs
+++ Program.cs
@@ -1,6 +1,22 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Mensagem Alterada!");
+var connectionSting = app.Configuration.GetConnectionString("DefaultConnection");
+
+var secrets = new Secrets();
+app.Configuration.GetSection("Secrets").Bind(secrets);
+
+app.MapGet("/", () => new {
+    ConnectionSting = connectionSting,
+    Secrets = secrets,
+    ApiUrl = app.Configuration.GetValue<string>("ApiUrl")
+});
 
 app.Run();
+
+public class Secrets
+{
+    public string JwtTokenSecret { get; set;} = string.Empty;
+    public string ApiKey { get; set;} = string.Empty;
+    public string PrivateKey { get; set;} = string.Empty;
+}
